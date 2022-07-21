6b55e371d04c060cd086fd5f30bc96ae42f1c6e7
AakashNarwade <aakashnarwade06@gmail.com>
2022-07-21 13:37:08 +0530

    removed console logs

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -11,16 +11,6 @@ const ProgressBar = (props) => {
       <div>progrees bar</div>
       <Progress completed={props.completed} />
     </div>
-    // <div>
-    //   <div className="number"></div>
-    //   {/* <img src={image} alt={name} /> */}
-    //   <div className="detail-wrapper">
-    //     <div>{/* <small>#0{id}</small> */}</div>
-    //     <Progress completed={55} />
-    //     {/* <h3>{name}</h3>
-    //   <h3>Type: {type}</h3> */}
-    //   </div>
-    // </div>
   );
 };
 
